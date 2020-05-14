15a3a6d4d41b0c102e186229e212337f54897bdf
sidereus <francesco.serafin.3@gmail.com>
2020-05-14 17:58:59 -0600

    cha: multithreading + clean up

--- coloramp/coloramp.py
+++ coloramp/coloramp.py
@@ -2,6 +2,9 @@ from qgis.core import QgsStyle, QgsProject, QgsSymbol, QgsRendererRange, QgsGrad
 from PyQt5.QtGui import QColor
 
 import math
+import threading
+import queue
+import time
 
 def linear_color_interpolation(c1, c2, t):
     r = int(c1.red() + (c2.red() - c1.red()) * t)
@@ -22,54 +25,98 @@ def getCRPlayers(fld):
             actualayers.append(layer)
     return actualayers
 
-def style(fld, colorName = 'PuRd', interval = 5):
+def getMaxParallelVal(layers, fld):
+
+    q = queue.Queue()
+    maxq = []
+
+    def worker():
+        while True:
+            layer = q.get()
+            fld_index = layer.fields().lookupField(fld)
+            lmax = layer.maximumValue(fld_index)
+            maxq.append(lmax)
+            q.task_done()
+
+    threading.Thread(target=worker, daemon=True).start()
+
+    for layer in layers:
+        q.put(layer)
+
+    q.join()
+    return max(maxq)
+
+def getMaxVal(layers, fld, multi):
+    if multi:
+        return getMaxParallelVal(layers, fld)
+    else:
+        upper = -1.0
+        for layer in layers:
+            fld_index = layer.fields().lookupField(fld)
+            lmax = layer.maximumValue(fld_index)
+            if lmax > upper:
+                upper = lmax
+        return upper
+
+def getIntervalInterpolation(nn):
+    t_list = []
+    interp = 0
+    for i in range(0, nn):
+        t_list.append(interp)
+        interp += 1./(nn-1)
+    return t_list
+
+def getInterpolatedColors(nn, colorName):
     myStyle = QgsStyle().defaultStyle()
     ramp = myStyle.colorRamp(colorName)
     c1 = ramp.color1()
     c2 = ramp.color2()
-    actualayers = getCRPlayers(fld)
-    lower = 0.0
-    upper = -1.0
-    for layer in actualayers:
-        fld_index = layer.fields().lookupField(fld)
-        lmax = layer.maximumValue(fld_index)
-        if lmax > upper:
-            upper = lmax
+    t_list = getIntervalInterpolation(nn)
     colors = []
-    max_erosion = int(round_up(upper))
-    max_erosion += interval
-    t_list = []
-    init = 0
-    number_classes = range(0, max_erosion, interval)
-    nn = max_erosion/interval
-    for i in number_classes:
-        t_list.append(init)
-        init += 1./(nn-1)
     for i in t_list:
         colors.append(linear_color_interpolation(c1, c2, i))
-    for layer in actualayers:
+    return colors
+
+def renderingUpdate(layers, colors, interval, fld):
+    for layer in layers:
         range_list = []
-        ll = lower
+        lower = 0.0
         for c in colors:
-            cat = [ll, ll + interval, c.name()]
+            cat = [lower, lower + interval, c.name()]
             sym = QgsSymbol.defaultSymbol(layer.geometryType())
             sym.setColor(c)
             sym.symbolLayer(0).setStrokeStyle(Qt.PenStyle(Qt.NoPen))
             rng = QgsRendererRange(cat[0], cat[1], sym, '{0:.1f}-{1:.1f}'.format(cat[0], cat[1]))
             range_list.append(rng)
-            ll = ll + interval
+            lower = lower + interval
         renderer = QgsGraduatedSymbolRenderer(fld, range_list)
         renderer.setMode(QgsGraduatedSymbolRenderer.Custom)
         layer.setRenderer(renderer)
         layer.triggerRepaint()
 
+def style(fld, multi, colorName = 'PuRd', interval = 5):
+
+    layers = getCRPlayers(fld)
+    t = time.time()
+    upper = getMaxVal(layers, fld, multi)
+    elapsed = time.time() - t
+    print(elapsed)
+    print(upper)
+
+    nn = int(round_up(upper/interval))
+    print(nn)
+    colors = getInterpolatedColors(nn, colorName)
+    renderingUpdate(layers, colors, interval, fld)
 
 def style_na(fld):
-    actualayers = getCRPlayers(fld)
+    layers = getCRPlayers(fld)
     c = '#20a802'
+    colors = []
+    colors.append(QColor(c))
     lmin = -1
     lmax = 0
-    for layer in actualayers:
+
+    for layer in layers:
         range_list = []
         cat = [lmin, lmax, c]
         sym = QgsSymbol.defaultSymbol(layer.geometryType())
